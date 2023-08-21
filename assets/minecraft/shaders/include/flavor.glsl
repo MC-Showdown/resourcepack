a73a54b74d3fc886bcb9af6013e6554bb4a33154
AFuxy <zakygames701@gmail.com>
2023-08-21 12:10:19 +0100

    V2.11.13

---
+++ assets/minecraft/shaders/include/flavor.glsl
@@ -0,0 +1,10 @@
+
+// Color of the outline on items when hovering over them in the GUI
+#define HOVER_OUTLINE_COLOR vec4(1.,0.608,0., 1.0)
+
+// Color of the border around tooltips
+#define TOOLTIP_OUTLINE_COLOR vec4(1.,0.608,0., 1.0)
+
+// Loading screen background color
+#define LOADING_BG_DARK_COLOR vec3(0.322,0.,0.)
+#define LOADING_BG_COLOR vec3(0.322,0.,0.)
